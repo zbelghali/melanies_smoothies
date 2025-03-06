a10eb8a85dc641d0bf456c2223bd6758835e5f91
zbelghali <10159001+zbelghali@users.noreply.github.com>
2025-03-06 10:38:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ name_on_order =st.text_input("Name on Smoothie")
 st.write(f'The name of the smoothie will be:{name_on_order}')
 
 st.write(st.secrets)
+st.write(st.secrets["connections"])
 
 
 # Retrieve the Base64 encoded private key from Streamlit secrets
