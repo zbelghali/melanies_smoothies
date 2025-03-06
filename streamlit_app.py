249679dffdae81593f484d869371478f36d9917a
zbelghali <10159001+zbelghali@users.noreply.github.com>
2025-03-06 10:47:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,7 @@ encoded_private_key = st.secrets["connections"]["snowflake"]["private_key"]
 # Decode the Base64 string back to the original private key binary
 private_key = base64.b64decode(encoded_private_key)
 
-cnx = st.connection("snowflake", type="snowflake", private_key=private_key, passphrase='snowflake')
-session=cnx.session()
+st.write(private_key)
+
+# cnx = st.connection("snowflake", type="snowflake", private_key=private_key, passphrase='snowflake')
+# session=cnx.session()
