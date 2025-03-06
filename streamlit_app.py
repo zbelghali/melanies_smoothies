963674e1f8f003539832494d5f69303d12947147
zbelghali <10159001+zbelghali@users.noreply.github.com>
2025-03-06 10:36:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,35 +10,11 @@ Choose the fruits you want in your custom Smoothie!""")
 name_on_order =st.text_input("Name on Smoothie")
 st.write(f'The name of the smoothie will be:{name_on_order}')
 
+st.write(st.secrets)
+
+
 # Retrieve the Base64 encoded private key from Streamlit secrets
-encoded_private_key = st.secrets["connections.snowflake"]["private_key"]
+# encoded_private_key = st.secrets["connections.snowflake"]["private_key"]
 
 # Decode the Base64 string back to the original private key binary
-private_key = base64.b64decode(encoded_private_key)
-
-session = get_active_session()
-cnx = st.connection("snowflake", type="snowflake", private_key=private_key, passphrase='snowflake')
-session=cnx.session()
-
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-
-# st.dataframe(data=my_dataframe, use_container_width=True)
-ingredient_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
-
-if ingredient_list:
-    ingredient_sring=''
-    for x in ingredient_list:
-        ingredient_sring += x + ' '
-
-    st.write(ingredient_sring)
-
-    my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
-    values ('{ingredient_sring}', '{name_on_order}')"""
-
-    # st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!')
+# private_key = base64.b64decode(encoded_private_key)
